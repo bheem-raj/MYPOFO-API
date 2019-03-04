2e84f7c18ebf04ef584474bd34be5de10d8b180e
bheem-raj <psbheemaraj93@gmail.com>
2019-03-04 19:49:55 +0530

    initial commit

---
+++ models/projectSchema.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+//schema accepts 
+
+const projectSchema = new Schema({
+
+    name: {
+        type: String, 
+        minlength:2, 
+        validate:{
+            validator: function(value) {
+                 // complete logic and then return true or false
+
+                return value.length > 3
+            },
+            message:'Minimum length should be 4'
+        },
+        required:true
+    },
+    alias: {type:String, unique:true, required:true},
+    tags: [String],
+    description: String,
+    githubUrl : {type:String, trim:true},
+    imageUrl: String,
+    relatedProjects:[{name: String, link: String}],
+    createdOn : {type: Date, default: Date.now()},
+    updatedOn : {type: Date}
+});
+
+
+
+
+
+
+
+//Returns complete project collection
+
+module.exports = mongoose.model('projects',projectSchema);
